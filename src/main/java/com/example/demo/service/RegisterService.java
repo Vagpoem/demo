2de7c0aad4750d2dd60cae59a019673adc691c90
Vagpoem <1427038873@qq.com>
2020-12-18 22:34:33 +0800

    注册功能实现（通过维护session和cookie信息实现登录状态的维护）-12-18

--- src/main/java/com/example/demo/service/RegisterService.java
+++ src/main/java/com/example/demo/service/RegisterService.java
@@ -12,7 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
-import javax.servlet.http.Cookie;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
