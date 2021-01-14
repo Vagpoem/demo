3c60a47f3975c3659a86f58e166ddc9e9abd2524
Vagpoem <1427038873@qq.com>
2021-01-14 11:23:19 +0800

    多人分发、投票-01-14

--- src/main/java/com/example/demo/util/VoteUtil.java
+++ src/main/java/com/example/demo/util/VoteUtil.java
@@ -7,4 +7,14 @@ public class VoteUtil {
 
         return res;
     }
+
+    public static void main(String[] args) {
+        String str = "54,100   139,94   182,142";
+        System.out.println(str.length());
+        String[] a = str.split("  ");
+        for (String s : a){
+            System.out.println(s);
+        }
+//        System.out.println(str.split("  "));
+    }
 }
